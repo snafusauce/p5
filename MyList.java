0264f4955b35463dfcf6575c548b0e53ccd0cc90
Michael Saucedo <mikesauce@Michaels-MBP-3.attlocal.net>
2022-01-14 22:52:32 -0800

    g

--- MyList.java
+++ MyList.java
@@ -1,5 +1,30 @@
-package p5;
+/**
+ * MyList is the superclass for all of it's derived 
+ * subclasses. You are given the basic interface that 
+ * every MyList must support.
+ */
+public interface MyList<T> {
+    public boolean add(T element);
 
-public class MyList {
-    
+    public void add(int index, T element);
+
+    public void clear();
+
+    public boolean contains(T element);
+
+    public T get(int index);
+
+    public int indexOf(T element);
+
+    public boolean isEmpty();
+
+    public int lastIndexOf(T element);
+
+    public T remove(int index);
+
+    public boolean remove(T element);
+
+    public T set(int index, T element);
+
+    public int size();
 }
